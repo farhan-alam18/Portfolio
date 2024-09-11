49ce4d32f7043d4baf40ce22242862bcaa1dc514
farhan-alam18 <farhanalam1113@gmail.com>
2024-09-11 21:59:39 +0530

    Contact Section Done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { AboutSection } from "@/sections/About";
+import { ContactSection } from "@/sections/Contact";
 import { Header } from "@/sections/Header";
 import { HeroSection } from "@/sections/Hero";
 import { ProjectsSection } from "@/sections/Projects";
@@ -12,6 +13,7 @@ export default function Home() {
       <ProjectsSection />
       <TapeSection />
       <AboutSection />
+      <ContactSection />
     </div>
   );
 }
--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,3 +1,37 @@
+import Link from "next/link";
+import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
+import grainImage from "@/assets/images/grain.jpg";
+
 export const ContactSection = () => {
-  return <div>Contact Section</div>;
+  return (
+    <div className="py-1 pt-12">
+      <div className="container">
+        <div className="bg-gradient-to-r from-emerald-400 to-sky-300 text-gray-900 py-8 px-10 rounded-3xl text-center relative overflow-hidden z-0">
+          <div
+            className="absolute inset-0 opacity-5 -z-10"
+            style={{
+              backgroundImage: `url(${grainImage.src})`,
+            }}
+          ></div>
+          <div className="flex flex-col md:flex-row gap-8 md:gap-16 items-center">
+            <div className="md:text-start">
+              <h2 className="font-serif text-2xl md:text-3xl">
+                Let&apos;s team up for success
+              </h2>
+              <p className="text-sm md:text-base mt-2 ">
+                Looking to tackle your next big project? Let&apos; connect
+                andexplore how I can contribute to reaching your goals.
+              </p>
+            </div>
+            <button className="text-white bg-gray-900  px-6 h-12 rounded-xl mt-8">
+              <Link href="" className="inline-flex items-center gap-2 w-max">
+                <span className="font-semibold">Contact Me</span>
+                <ArrowUpRightIcon className="size-5" />
+              </Link>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
